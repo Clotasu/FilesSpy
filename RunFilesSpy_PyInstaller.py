212d38485062dcbe3f26806b6464304cab21d6b5
Clotasu <144500846+Clotasu@users.noreply.github.com>
2023-12-01 00:38:20 +0800

    Update RunFilesSpy_PyInstaller.py

--- RunFilesSpy_PyInstaller.py
+++ RunFilesSpy_PyInstaller.py
@@ -24,11 +24,11 @@ if mode == '1':
 elif mode == '2':
     current_system = platform.system()
     if current_system == 'Windows':
-        subprocess.Popen([pythonw_path, py_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
+        subprocess.Popen([python_path, py_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
     elif current_system == 'Linux':
         subprocess.Popen([python_path, py_file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, stdin=subprocess.PIPE, close_fds=True)
     elif current_system == 'Darwin':  # macOS
-        subprocess.Popen([pythonw_path, py_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, preexec_fn=os.setpgrp)
+        subprocess.Popen([python_path, py_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, preexec_fn=os.setpgrp)
     else:
         print("不支持的操作系统。")
 else:
